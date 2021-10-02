d6b133540234a7e939ee5ac16af4f61bab436357
Luke <lukewfisher@msn.com>
2021-10-02 10:34:44 -0600

    player movement is locked

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -22,6 +22,7 @@ using extensions.FlxStateExt;
 
 class PlayState extends FlxTransitionableState {
 	var player:FlxSprite;
+	var playerMovementLock: Float = 0;
 
 	var test:DepthSprite;
 
@@ -59,25 +60,56 @@ class PlayState extends FlxTransitionableState {
 		// test.angle +=  30 * elapsed;
 		// camera.angle +=  15 * elapsed;
 
-		if(SimpleController.just_pressed(Button.RIGHT)){
+		if(playerMovementLock == 0){
+			if(SimpleController.just_pressed(Button.RIGHT)){
 
-		}
+			}
 
-		var cardinalInput = InputCalcuator.getInputCardinal();
-		FlxG.watch.addQuick("the mofo input: ",cardinalInput);
-		if(cardinalInput != Cardinal.NONE){
-			var vectorInput = cardinalInput.asVector().scale(16);//maybe make tile size configurable
+			var cardinalInput = InputCalcuator.getInputCardinal();
+			FlxG.watch.addQuick("the mofo input: ",cardinalInput);
+			if(cardinalInput != Cardinal.NONE){
+				var vectorInput = cardinalInput.asVector().scale(16);//maybe make tile size configurable
 
-			var targetTile = player.getMidpoint().addPoint(vectorInput);
-			FlxG.watch.addQuick("the mofo tile: ",targetTile);
+				var targetTile = player.getMidpoint().addPoint(vectorInput);
+				FlxG.watch.addQuick("the mofo tile: ",targetTile);
 
-			var collidedBLocks = playerCollidables.members.filter(block -> block.overlapsPoint(targetTile));
+				//check the blocks that could be stopping the player and find the one that is at the location of where the player is trying to move to
+				var collidedBlocks = playerCollidables.members.filter(block -> block.overlapsPoint(targetTile));
 
-			if(collidedBLocks.length == 0 ){
-				FlxTween.linearMotion(player, player.x, player.y, targetTile.x -8 , targetTile.y -8);//limit player input, add more collisins
+				//if there's nothing in the way
+				if(collidedBlocks.length == 0 ){
+					FlxTween.linearMotion(player, player.x, player.y, targetTile.x -8 , targetTile.y -8);//limit player input, add more collisins
+					playerMovementLock =  1; 
+				}
+				else{//something is in front of the player
+					var blockingBlock = collidedBlocks[0];
+
+					//check to see if the block in the way is pushable
+					if(blockingBlock.pushable){
+
+						//find the direction/tile that would be blocking the pushable that is blocking the player
+						var vectorInput2 = cardinalInput.asVector().scale(32);
+						var targetTile2 = player.getMidpoint().addPoint(vectorInput2);
+						//check if there is a block in the way of the block trying to be pushed
+						var collidedBlocks2 = collidables.members.filter(block -> block.overlapsPoint(targetTile2));
+
+						if(collidedBlocks2.length == 0 && playerMovementLock == 0){// there is nothing in front of the pushable block
+							FlxTween.linearMotion(blockingBlock, blockingBlock.x, blockingBlock.y, targetTile2.x -8 , targetTile2.y -8);//push the block to the new space
+							FlxTween.linearMotion(player, player.x, player.y, targetTile.x -8 , targetTile.y -8);//move the player into the pushed blocks space
+							playerMovementLock =  1; 
+						}
+
+					}
+				}
+
 			}
 
 		}
+		if(playerMovementLock >0){
+			playerMovementLock -= elapsed;
+			if(playerMovementLock <0) playerMovementLock = 0;
+		}
+		FlxG.watch.addQuick("the lock: ",playerMovementLock);
 
 	}
 
