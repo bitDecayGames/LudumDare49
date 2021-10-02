a4a966085d3872619a210cc5eeffb9e1fe358985
Monday <monday.hopscotch@gmail.com>
2021-10-02 12:06:27 -0600

    more helpers for art. cooler block

--- source/Main.hx
+++ source/Main.hx
@@ -22,6 +22,10 @@ import states.PlayState;
 import states.LoganState;
 #end
 
+#if model
+import states.ModelTest;
+#end
+
 class Main extends Sprite {
 	public function new() {
 		super();
@@ -40,6 +44,10 @@ class Main extends Sprite {
 
 		#if logan
 		startingState = LoganState;
+		#end
+
+		#if model
+		startingState = ModelTest;
 		#end
 
 		addChild(new FlxGame(Std.int(640 / 1), Std.int(480 / 1), startingState, 1, 60, 60, true, false));
