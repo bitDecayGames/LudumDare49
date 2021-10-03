cbdd7fa4f0b32a22336a8b5313c2ae8c7382ea76
Tanner <you@example.com>
2021-10-02 19:28:33 -0600

    working on cutscene more

--- source/FmodConstants.hx
+++ source/FmodConstants.hx
@@ -16,4 +16,5 @@ class FmodSFX {
     public static inline var Typewriter:String = "event:/SFX/Hud/Typewriter";
     public static inline var MenuHover:String = "event:/SFX/Menu/MenuHover";
     public static inline var MenuSelect:String = "event:/SFX/Menu/MenuSelect";
+    public static inline var PowerUp:String = "event:/SFX/Pocobot/PowerUp";
 }
--- source/states/TannerState.hx
+++ source/states/TannerState.hx
@@ -1,5 +1,7 @@
 package states;
 
+import flixel.tweens.misc.VarTween;
+import flixel.util.FlxTimer;
 import ui.camera.SetupCameras;
 import helpers.Constants;
 import systems.ControlSystem;
@@ -44,10 +46,15 @@ class TannerState extends FlxTransitionableState {
 	var collidables:FlxTypedGroup<FlxSprite> = new FlxTypedGroup();
 	var nonCollidables:FlxTypedGroup<FlxSprite> = new FlxTypedGroup();
 
+	var turnedOn = false;
+
+	var tween:VarTween;
+
 	// SFX
 	var typeSoundId = "Typewriter";
 	var sirenId = "siren";
 	var lowPass = false;
+	var sirensOff = false;
 
 	override public function create() {
 		super.create();
@@ -64,16 +71,22 @@ class TannerState extends FlxTransitionableState {
 
 		var dialgs = new Map<String, Array<String>>();
 		dialgs["Intro"] = ["Intercom: System power at 0%", "Intercom: Meltdown imminent", "Intercom: Engaging all available Pocobots to recharge facitility power cores",
-		 "Pocobot: ...", "Pocobot: .................powering on", "Pocobot: beep boop", "Pocobot: Power core straight ahead", "I must do my duty"];
+		 "Pocobot: ...", "Pocobot: .................powering on", "Pocobot: beep boop", "Pocobot: Power core straight ahead", "Pocobot: I must do my duty"];
 		dialogManager = new DialogManager(dialgs, this, SetupCameras.uiCamera, FlxKey.SPACE, 
 			() -> {FmodManager.PlaySoundAndAssignId(FmodSFX.Typewriter, typeSoundId);}, 
 			() -> {FmodManager.StopSoundImmediately(typeSoundId);});
 		dialogManager.loadDialog("Intro");
 
 
+		player = new Player(50, 50);
+		playerOff = new Player(50, 50, AssetPaths.player_off__png);
+		
+		collidables.add(player);
+		collidables.add(playerOff);
+
 		var wall = new Wall(669 + Constants.TILE_SIZE, 50);
 		var radBlock = new RadioactiveBlock(1, 1000);
-		radBlock.setPosition(669, 50);
+		radBlock.setPosition(player.x-48, player.y);
 
 		var radCooler = new RadioactiveCooler();
 		radCooler.setPosition(669+(Constants.TILE_SIZE * 3), 50);
@@ -91,12 +104,6 @@ class TannerState extends FlxTransitionableState {
 		playerCollidables.add(radCooler);
 
 		nonCollidables.add(conveyor);
-
-		player = new Player(50, 50);
-		playerOff = new Player(50, 50, AssetPaths.player_off__png);
-		
-		collidables.add(player);
-		collidables.add(playerOff);
 
 
 		// test = new DepthSprite(32, 32);
@@ -119,12 +126,8 @@ class TannerState extends FlxTransitionableState {
 
 		FmodManager.PlaySong(FmodSongs.EmergencyPowerActivated);
 		FmodManager.PlaySoundWithReference(FmodSFX.Siren);	
-		FlxTween.tween(sirenLight, {alpha: 0}, 2, {
+		tween = FlxTween.tween(sirenLight, {alpha: 0}, 2, {
 			type: FlxTweenType.LOOPING,
-			onComplete: function(_)
-			{
-				FmodManager.PlaySoundAndAssignId(FmodSFX.Siren, sirenId);	
-			}
 		});
 		
         FlxG.camera.zoom = 2;
@@ -144,11 +147,30 @@ class TannerState extends FlxTransitionableState {
 			}
 			lowPass = true;
 		}
+
+		if (FlxG.keys.pressed.Z){
+			FmodManager.StopSongImmediately();
+			FmodManager.StopSoundImmediately(sirenId);
+			sirenLight.visible = false;
+		}
+
+		if(dialogManager.getCurrentDialogPage() == 4) { 
+			if(!turnedOn){
+				new FlxTimer().start(2).onComplete = function(t:FlxTimer) {
+					playerOff.visible = false;
+					FmodManager.PlaySoundOneShot(FmodSFX.PowerUp);
+				}
+				turnedOn = true;
+			}
+		}
+
 		if(dialogManager.getCurrentDialogPage() == 8) {
 			if(lowPass) {
 				FlxTween.tween(camera, {zoom: 2}, 1, {});
 			}
 			lowPass = false;
+			controlSystem.playerIscontrollable = true;
+			tween.destroy();
 		}
 
 		if(lowPass){
