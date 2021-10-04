68ef1a33c9c2334ca6266c08497570cb5850502e
mwingfield <mwingfie@gmail.com>
2021-10-03 19:44:17 -0600

    tooltip

---
+++ source/ui/counter/Tooltip.hx
@@ -0,0 +1,53 @@
+package ui.counter;
+
+import flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup;
+import depth.DepthSprite;
+import misc.FlxTextFactory;
+import flixel.FlxSprite;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.FlxObject;
+import flixel.text.FlxText;
+
+class Tooltip extends FlxTypedSpriteGroup<FlxSprite> {
+	private static var yOffset = -20.0;
+
+	private var follow:FlxObject;
+	private var text:FlxText;
+	private var arrow:DepthSprite;
+	private var zVel:Float;
+
+	public function new(x:Float, y:Float, text:String) {
+		super();
+		this.text = FlxTextFactory.make(text, 0, 0, 10, FlxTextAlign.CENTER);
+		add(this.text);
+		arrow = new DepthSprite(0, 0);
+		arrow.load_slices(AssetPaths.arrow__png, 16, 16, 6);
+		arrow.slice_offset = 1.5;
+		add(arrow);
+		this.x = x;
+		this.y = y;
+		zVel = 0;
+		arrow.z = -10;
+	}
+
+	public function setFollow(obj:FlxObject):Tooltip {
+		follow = obj;
+		return this;
+	}
+
+	public override function update(elapsed:Float) {
+		super.update(elapsed);
+		if (follow != null) {
+			x = follow.x;
+			y = follow.y + yOffset;
+		}
+		text.angle = 360 - camera.angle;
+		arrow.angle += 1;
+		zVel += 0.1;
+		arrow.z += zVel;
+		if (arrow.z > 0) {
+			arrow.z = 0;
+			zVel *= -1;
+		}
+	}
+}
