59c8ded9c3446cd746cf21f1eb3e60c143309f4c
Jacob Fisher <fishervjacob@gmail.com>
2021-10-04 14:09:30 -0600

    Num Moves stats

--- source/systems/MovementSystem.hx
+++ source/systems/MovementSystem.hx
@@ -45,6 +45,7 @@ class MovementSystem extends StateSystem {
 
 			// if there's nothing in the way
 			if (collidedBlocks.length == 0) {
+				ControlSystem.incrementMovements();
 				player.setDir(cardinalInput);
 				FlxTween.linearMotion(player, player.x, player.y, targetTile.x - 8, targetTile.y - 8,
 					Constants.PLAYER_SPEED); // limit player input, add more collisins
@@ -58,6 +59,7 @@ class MovementSystem extends StateSystem {
 
 				if (!blockingBlock.isActive) {
 					// if block is dead you can move into it
+					ControlSystem.incrementMovements();
 					player.setDir(cardinalInput);
 					FlxTween.linearMotion(player, player.x, player.y, targetTile.x - 8, targetTile.y - 8,
 						Constants.PLAYER_SPEED); // limit player input, add more collisins
@@ -73,6 +75,7 @@ class MovementSystem extends StateSystem {
 					// check if there is a block in the way of the block trying to be pushed
 					var collidedBlocks2 = collidables.members.filter(block -> block.overlapsPoint(targetTile2));
 					if (collidedBlocks2.length == 0) {
+						ControlSystem.incrementMovements();
 						player.setDir(cardinalInput);
 						FlxTween.linearMotion(blockingBlock, blockingBlock.x, blockingBlock.y, targetTile2.x - 8, targetTile2.y - 8, runningTimeDuration);
 						FlxTween.linearMotion(player, player.x, player.y, targetTile.x - 8, targetTile.y - 8, runningTimeDuration);
